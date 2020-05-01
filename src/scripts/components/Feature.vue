c679757ad953513c4425e961d916d7f6fa7a5d23
Jeric Epon <jericepon@gmail.com>
2020-05-01 15:14:38 +0800

    Feature section WIP

---
+++ src/scripts/components/Feature.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="feature">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <FeatherIcon />
+            <div class="feature-title">Optimized Website</div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum dolor, tellus facilisis consequat, nibh. Velit tellus, nisl elementum aliquam nunc, ut tristique faucibus dignissim.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <CodeIcon />
+            <div class="feature-title">Pixel-Perfect & Clean Code</div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum dolor, tellus facilisis consequat, nibh. Velit tellus, nisl elementum aliquam nunc, ut tristique faucibus dignissim.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <ResponsiveIcon />
+            <div class="feature-title">Resposive & Mobile First</div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum dolor, tellus facilisis consequat, nibh. Velit tellus, nisl elementum aliquam nunc, ut tristique faucibus dignissim.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Feature'
+}
+</script>
