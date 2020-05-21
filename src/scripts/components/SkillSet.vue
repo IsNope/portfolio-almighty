91907c5f630e26cbc6f8b4949bdbf15293ff3371
Jeric Epon <jeric.epon@stokmanila.com>
2020-05-21 15:18:40 +0800

    Skillset section

--- src/scripts/components/SkillSet.vue
+++ src/scripts/components/SkillSet.vue
@@ -2,21 +2,21 @@
   <section class="skillset">
     <div class="container">
       <div class="row">
-        <div class="col-md-3 d-flex justify-content-center align-items-center order-md-0 order-1">
-          <div class="d-block">
+        <div class="col-md-3 d-flex justify-content-lg-center align-items-center order-md-0 order-1 py-lg-0 py-4">
+          <div class="d-block mx-auto skillset-inner text-lg-left text-center">
             <div class="skillset-title h2">Front-End Developer</div>
             <div class="skillset-body">
               <p class="mb-0" v-for="(skill, index) in frontEndSkills" :key="index">{{skill}}</p>
             </div>
           </div>
         </div>
-        <div class="col-md-6 order-md-1 order-0">
+        <div class="col-md-6 order-md-1 order-0 mb-lg-0 mb-4">
           <figure>
             <PieIcon />
           </figure>
         </div>
-        <div class="col-md-3 d-flex justify-content-center align-items-center order-md-2 order-2">
-          <div class="d-block">
+        <div class="col-md-3 d-flex justify-content-lg-center align-items-center order-md-2 order-2 py-lg-0 py-4">
+          <div class="d-block mx-auto skillset-inner text-lg-left text-center">
             <div class="skillset-title h2">Designer</div>
             <p class="mb-0" v-for="(skill, index) in designerSkills" :key="index">{{skill}}</p>
           </div>
